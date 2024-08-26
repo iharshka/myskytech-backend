9a79da05d9bbdd133791e69b706d667f1483d3c4
Harsh Gautam <kanhaharsh7725@gmail.com>
2024-08-27 02:24:42 +0530

    Removed optForFollups from ZOD schema

--- types.js
+++ types.js
@@ -2,10 +2,9 @@ const zod = require("zod");
 
 const clientSchema = zod.object({
     name: zod.string(),
-    email: zod.string(),
+    email: zod.string().email({ message: "Invalid email address" }),
     tel: zod.number(),
     msg: zod.string(),
-    optForFollowups: zod.boolean(),
 })
 
 module.exports = {
